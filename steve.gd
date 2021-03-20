40691e7770315baefeeb048c9d61a793e5660528
Nkwua Precious <nkwuap@gmail.com>
2021-03-20 17:03:10 +0100

    still moving

--- steve.gd
+++ steve.gd
@@ -1,6 +1,21 @@
 extends KinematicBody2D
 
 var velocity=Vector2(0,0)
+const SPEED=180
+const FORCEJUMP=-900
+const GRAVITY=30
 
 func _physics_process(delta):
-	print("hello youtubers")
+	if Input.is_action_pressed("right"):
+		velocity.x=SPEED
+	if Input.is_action_pressed("left"):
+		velocity.x=-SPEED
+		
+	velocity.y+=GRAVITY	
+	
+	if Input.is_action_pressed("jump"):
+		velocity.y=FORCEJUMP
+	
+	velocity=move_and_slide(velocity)
+	
+	velocity.x=lerp(velocity.x,0,0.1)
