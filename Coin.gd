62d89a056aa3aceca3975185936328cf5f48536c
Nkwua Precious <nkwuap@gmail.com>
2021-03-23 18:41:27 +0100

    adding coin counters

--- Coin.gd
+++ Coin.gd
@@ -1,12 +1,10 @@
 extends Area2D
 
-
+signal coin_collected
 
 func _on_Coin_body_entered(body):
-	
 	$AnimationPlayer.play("bounce")
-	body.add_Coin()
-
+	emit_signal("coin_collected")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	queue_free()
---
+++ HUD.gd
@@ -0,0 +1,6 @@
+extends CanvasLayer
+
+var coins=0
+
+func _ready():
+	$Coins.text=String(coins)
