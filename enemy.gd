1b61b1f56b7ca3910248e7000b2f3a7fc42d921a
Nkwua Precious <nkwuap@gmail.com>
2021-03-24 23:18:14 +0100

    direction and masking set

--- enemy.gd
+++ enemy.gd
@@ -4,12 +4,18 @@ var velocity=Vector2()
 export var direction=-1
 
 func _ready():
-	pass
+	if direction==1:
+		$Sprite.flip_h=true
 	
 	
 func _physics_process(delta):
+	
+	if is_on_wall():
+		direction*=-1
+		$Sprite.flip_h=not $Sprite.flip_h
+	
 	velocity.y+=20
 	
 	velocity.x=50*direction
 	
-	move_and_slide(velocity,Vector2.UP)
+	velocity=move_and_slide(velocity,Vector2.UP)
