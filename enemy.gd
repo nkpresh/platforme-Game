21dcf3ae3b2f2283a91c5647ac6285805f20f08d
Nkwua Precious <nkwuap@gmail.com>
2021-03-24 23:42:49 +0100

    cliff detection

--- enemy.gd
+++ enemy.gd
@@ -2,16 +2,18 @@ extends KinematicBody2D
 
 var velocity=Vector2()
 export var direction=-1
+export var detects_cliffs=true
 
 func _ready():
 	if direction==1:
 		$Sprite.flip_h=true
 	$floor_Checker.position.x=$CollisionShape2D.shape.get_extents().x*direction
+	$floor_Checker.enabled=detects_cliffs
 	
 	
 func _physics_process(delta):
 	
-	if is_on_wall():
+	if is_on_wall() or not $floor_Checker.is_colliding() and is_on_floor():
 		direction*=-1
 		$Sprite.flip_h=not $Sprite.flip_h
 		$floor_Checker.position.x=$CollisionShape2D.shape.get_extents().x*direction
