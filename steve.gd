cae1be5d04574b25d3468f9c8aea76947f7c41af
Nkwua Precious <nkwuap@gmail.com>
2021-03-27 12:05:51 +0100

    sounds

--- steve.gd
+++ steve.gd
@@ -24,6 +24,7 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y=FORCEJUMP
+		$SoundJump.play()
 	
 	velocity=move_and_slide(velocity,Vector2.UP)
 	
