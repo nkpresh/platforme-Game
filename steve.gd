57ba96bb898e8cd189b28de913801bdb887f6633
Nkwua Precious <nkwuap@gmail.com>
2021-03-27 14:29:27 +0100

    done and dusted

--- steve.gd
+++ steve.gd
@@ -5,6 +5,7 @@ var Coins=0
 const SPEED=250
 const FORCEJUMP=-1100
 const GRAVITY=35
+var ouchPoint=3
 
 func _physics_process(delta):
 	if Input.is_action_pressed("right"):
@@ -40,6 +41,8 @@ func bounce():
 	velocity.y=FORCEJUMP*0.5
 	
 func ouch(var enemyposx):
+	ouchPoint-=1
+	$TextureTimer.start(3)
 	set_modulate(Color(1,0.3,0.3,0.3))
 	velocity.y=FORCEJUMP*0.5
 	
@@ -49,9 +52,16 @@ func ouch(var enemyposx):
 		velocity.x=800
 	Input.action_release("left")
 	Input.action_release("right")
-	
-	$Timer.start()
+	$SoundOuch.play()
+	if(ouchPoint==0):
+		$Timer.start()		
+
 
 
 func _on_Timer_timeout():
 	get_tree().change_scene("res://GameOver.tscn")
+	
+
+
+func _on_TextureTimer_timeout():
+	set_modulate(Color(1,1,1,1))
